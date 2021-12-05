0ad15703452f45302c3421027e07c8b6a486a421
neparidze <neparidze@lanit.ru>
2021-12-05 21:39:20 +0300

    Added json loader and parse

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-var a = 0
+  function preload(arrayOfImages) {
+    for(i = 0 ; i < arrayOfImages.length ; i++)
+       {
+   var img = new Image();
+           img.src = arrayOfImages[i];
+       }
+  }
 
-console.log(a)
+preload(['/img/loader.gif']);
+
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('https://www.cbr-xml-daily.ru/daily_json.js',
+function(err, data) {
+  if (err !== null) {
+    console.log(err)
+  } else {
+      var json = JSON.parse(data);
+    console.log(json.Valute.USD.Value)
+  }
+});
