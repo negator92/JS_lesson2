aab68255dfd66c68affb4dbef27efdaddc55b73a
Vincent Neparidze <negator92@gmail.com>
2021-12-12 12:44:01 +0300

    Added canvas

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
-  function preload(arrayOfImages) {
+// function sleep(milliseconds) {
+//   const date = Date.now();
+//   let currentDate = null;
+//   do {
+//     currentDate = Date.now();
+//   } while (currentDate - date < milliseconds);
+// }
+
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context.strokeStyle = 'grey';
+context.lineWidth = 2;
+context.strokeRect(50, 50, window.innerWidth-100, window.innerHeight-100);
+
+function preload(arrayOfImages) {
     for(i = 0 ; i < arrayOfImages.length ; i++)
        {
    var img = new Image();
            img.src = arrayOfImages[i];
        }
-  }
+}
+var getElements = function(tagName, attribute, value, callback) {
+  var tags = window.document.getElementsByTagName(tagName);
+  for (var i=0; i < tags.length; i++) {
+    var tag = tags[i];
+    if (tag.getAttribute(attribute) == value) {
+      callback(tag);
+    }
+  };
+};
 
 preload(['/img/loader.gif']);
-
+// sleep(2200);
 
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
