ab3f8a44476eeb126f7bbcf177430b7856b493fd
Vincent Neparidze <negator92@gmail.com>
2021-12-12 20:05:02 +0300

    Added net core web empty to load localStorage ie11

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -7,8 +7,8 @@ function LoadNow(opacity) {
   else {
     loader.style.opacity = opacity;
     window.setTimeout(function () {
-      LoadNow(opacity - 0.05)
-    }, 50);
+      LoadNow(opacity - 0.01)
+    }, 15);
   }
 }
 
@@ -18,14 +18,13 @@ function displayContent(){
 }
 
 window.onload = function (){
-  var date = Date.now();
-  var curDate = null;
-  do {
-      curDate = Date.now();
-  } while (curDate-date < 2200);
   loader = document.getElementById('preloader');
   LoadNow(1);
 }
+
+var usd;
+localStorage.setItem('usd', '');
+console.log(usd)
 
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
@@ -42,12 +41,17 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 
-getJSON('https://www.cbr-xml-daily.ru/daily_json.js',
-function(err, data) {
+getJSON('https://www.cbr-xml-daily.ru/daily_json.js', function(err, data) {
   if (err !== null) {
     console.log(err)
   } else {
       var json = JSON.parse(data);
-    console.log(json.Valute.USD.Value)
-  }
-});
+      var usdValue = json.Valute.USD.Value;
+      usd = usdValue;
+      console.log(usd)
+      localStorage.setItem('usd', usdValue);
+    }
+  });
+console.log(usd)
+
+console.log(window.localStorage);
