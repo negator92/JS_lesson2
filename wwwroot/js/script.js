f7c710a32175a0a2e62c2d915f293532faffd0fa
neparidze <neparidze@lanit.ru>
2021-12-30 16:57:58 +0300

    Updates for es5, es6

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -2,17 +2,18 @@ var loader;
 var cnt;
 var myJson;
 localStorage.data;
+localStorage.loaded = false;
 localStorage.interval;
 
-window.onload = function() {
+window.onload = function () {
   loader = document.getElementById('preloader');
   reload();
 }
 
 function decreaseOpacity(opacity) {
-  if(opacity > 0) {
-      loader.style.opacity = opacity;
-      window.setTimeout(function () {
+  if (opacity > 0) {
+    loader.style.opacity = opacity;
+    window.setTimeout(function () {
       decreaseOpacity(opacity - 0.01)
     }, 10);
   }
@@ -21,34 +22,45 @@ function decreaseOpacity(opacity) {
   }
 }
 
-function displayLoader(){
+function checkFlag() {
+  if (localStorage.loaded == false) {
+    window.setTimeout(checkFlag, 100);
+  }
+}
+
+function displayLoader() {
   loader.style.display = 'block';
   cnt = document.getElementById('content');
   cnt.style.display = 'none';
 }
 
-function displayContent(){
+function displayContent() {
   loader.style.display = 'none';
   cnt = document.getElementById('content');
   cnt.style.display = 'block';
 }
 
-function loadCurency(){
-  getJSON('https://www.cbr-xml-daily.ru/daily_json.js', function(err, data) {
+function loadCurency() {
+  ;
+  getJSON('https://www.cbr-xml-daily.ru/daily_json.js', function (err, data) {
     if (err !== null) {
       console.log(err);
       alert(err);
+      loadCurency();
     } else {
-      localStorage.data = data;
+      localStorage.loaded = true;
+      localStorage.data = JSON.stringify(data);
+      return data;
     }
   });
 }
 
-var getJSON = function(url, callback) {
+function getJSON(url, callback) {
+  localStorage.loaded = false;
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'json';
-  xhr.onload = function() {
+  xhr.onload = function () {
     var status = xhr.status;
     if (status === 200) {
       callback(null, xhr.response);
@@ -64,14 +76,25 @@ function reload() {
     window.clearInterval(localStorage.interval);
   }
   loadData();
-  localStorage.interval = setInterval(loadData, 3000);
+  localStorage.interval = setInterval(loadData, 7200000); //7200000
 }
 
 function loadData() {
   displayLoader();
   loadCurency();
-  tableCreate();
-  decreaseOpacity(1);
+  checkFlag();
+  if (isJSON(localStorage.data)) {
+    tableCreate();
+    decreaseOpacity(1);
+  }
+}
+
+function isJSON(str) {
+  try {
+    return (JSON.parse(str) && !!str);
+  } catch (e) {
+    return false;
+  }
 }
 
 function tableCreate() {
@@ -96,12 +119,19 @@ function tableCreate() {
   var table = document.createElement('table');
   table.className = "table";
   cnt.appendChild(table);
-  var tblbody = "<thead><tr><th class='row-name'>Название</th><th class='row-valute'>Валюта</th><th class='row-value'>Цена</th><th class='row-previous'>Вчерашняя</th></th></thead>";
+  var tblbody = "<thead><tr><th class='row-name'>Название</th><th class='row-valute'>Валюта</th><th class='row-value'>Цена</th><th class='row-previous'>Вчерашняя</th></tr></thead>";
 
-  myJson = JSON.parse(localStorage.data);
+
+  if (typeof JSON.parse(localStorage.data) == 'string') {
+    var a = JSON.parse(localStorage.data);
+    myJson = JSON.parse(a);
+  }
+  else {
+    myJson = JSON.parse(localStorage.data);
+  }
   var valutes = myJson.Valute;
   for (key in valutes) {
-    tblbody += 
+    tblbody +=
       "<tr><td>" + valutes[key].Name
       + "</td><td>" + key
       + "</td><td>" + valutes[key].Value
